124f8721e4ec856badee0461ba0fa097de19e66f
yuanjuninfo <yuanjun.info@gmail.com>
2015-05-09 09:05:54 +0800

    Update and rename base.el6.x86_64.sh to install_base.el6.x86_64.sh

--- download/install_base.el6.x86_64.sh
+++ download/install_base.el6.x86_64.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # YUAN, Jun
 # 2015-05-09
-# base.el6.x86_64.sh
+# install_base.el6.x86_64.sh
 
 if [[ ${UID} = "0" ]];then
 echo -e "Need to be root [\e[1;32m OK \e[0m]"
@@ -20,42 +20,6 @@ echo -e "[\e[1;32m Install Development tools \e[0m] y/n"
 read -n 1 -t 5 deveinfo
 if [[ ${deveinfo} = "y" || -z ${deveinfo} ]];then
 yum groupinstall -y "Development tools"
-fi
-
-echo -e "[\e[1;32m To configure /etc/vimrc \e[0m] y/n"
-read -n 1 -t 5 vimrcinfo
-if [[ ${vimrcinfo} = "y" || -z ${vimrcinfo} ]];then
-cat >> /etc/vimrc <<eof
-set nocompatible
-set number
-filetype on
-set history=1000
-syntax on
-set autoindent
-set smartindent
-set tabstop=2
-set shiftwidth=2
-set showmatch
-set guioptions-=T
-set ruler
-set nohls
-set incsearch
-set nobackup
-set fileencodings=utf-8,gb
-set foldmethod=indent
-set foldlevel=100
-eof
-fi
-
-echo -e "[\e[1;32m To configure /etc/bashrc \e[0m] y/n"
-read -n 1 -t 5 bashrcinfo
-if [[ ${bashrcinfo} = "y" || -z ${bashrcinfo} ]];then
-cat >> /etc/bashrc <<eof
-# Alias set up
-alias ll='ls -lhF --color=auto'
-alias la='ls -alhF --color=auto'
-alias l='ls -1 --color=auto'
-eof
 fi
 
 echo -e "[\e[1;32m Update of this system now \e[0m] y/n"
